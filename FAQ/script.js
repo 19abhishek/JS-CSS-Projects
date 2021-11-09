310bc86700b2c36a76dcba0e69af0ee26ebc7e53
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-11-09 22:38:56 +0530

    FAQ collapse

--- FAQ/script.js
+++ FAQ/script.js
@@ -1,19 +1,23 @@
-const questions = document.querySelectorAll(".questions");
+const faqs = document.querySelectorAll(".faq");
 
-questions.forEach((question) => {
-  // const openIcon = question.querySelector(".open-icon");
-  // const answer = question.querySelector(".faq-answer");
-  // const closeIcon = document.querySelector(".close-icon");
-  // openIcon.addEventListener("click", open(openIcon, answer));
-  // closeIcon.addEventListener("click", close(answer, openIcon));
+faqs.forEach((faq) => {
+  // console.log(faq.childNodes);
+
+  faq.children[1].addEventListener("click", () => {
+    faq.children[3].classList.toggle("close");
+    //console.log(faq.children[3].children[1]);
+  });
+  faq.children[3].children[1].addEventListener("click", () => {
+    faq.children[3].classList.toggle("close");
+  });
 });
 
-function open(openIcon, answer) {
-  openIcon.classList.add("close");
-  answer.classList.add("close");
-}
+// function open(openIcon, answer) {
+//   openIcon.classList.add("close");
+//   answer.classList.add("close");
+// }
 
-function close(answer, openIcon) {
-  answer.classList.toggle("close");
-  openIcon.classList.toggle("close");
-}
+// function close(answer, openIcon) {
+//   answer.classList.toggle("close");
+//   openIcon.classList.toggle("close");
+// }
