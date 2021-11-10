c655463adf6823d9ee73ae6f13fae3306d56304e
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-11-10 23:54:44 +0530

    incrementing counter

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -4,16 +4,29 @@ counts.forEach((count) => {
   const follower = count.textContent;
   console.log(follower);
   let current = 0;
-  var interval = setInterval(counter, 1);
 
   function counter() {
-    if (current === follower) {
-      current = 0;
-      clearInterval(interval);
-      console.log("hello");
-    } else {
+    if (current < follower) {
       count.innerHTML = current;
       current++;
+      var interval = setInterval(counter, 100);
+    } else {
+      current = 0;
+      count.innerHTML = follower;
+      // clearInterval(interval);
+      // console.log("hello");
     }
+
+    // if (current === follower) {
+    //   current = 0;
+    //   clearInterval(interval);
+    //   console.log("hello");
+    // } else {
+    //   count.innerHTML = current;
+    //   current++;
+    //   setInterval(counter, 100);
+    // }
   }
+
+  counter();
 });
